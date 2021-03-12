3c7087033a0edec2dde2573cd9b566293eca8b2d
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-03-13 02:58:57 +0530

    adding serverless and intial testing code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('test');
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        hello: "hi!"
+    });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
