ca38a481070602b4efaa1391c5c3094109a6f729
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-03-16 21:21:25 +0530

    adding proper endpoint for sending email

--- src/email.js
+++ src/email.js
@@ -1,20 +1,26 @@
 const nodemailer = require("nodemailer");
 const dotenv = require('dotenv');
+const moment = require('moment');
 dotenv.config();
 
-let sendEmail = async function () {
+let sendEmail = async function (email) {
+    const today = new moment();
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.email,
-            pass: process.env.password
+            user: process.env.fromEmail,
+            pass: process.env.fromPassword
         }
     });
     let promise = await transporter.sendMail({
-        to: "vishal.vishalmishra.mishra2@gmail.com",
-        subject: "Freelancing Client",
-        text: "Hello world?",
-        html: "<b>Hello world?</b>",
+        to: process.env.sendToEmail,
+        subject: `Freelancing Client ${today.format('DD-MMM-YYYY')}`,
+        html: `<b>
+                <div style="margin-bottom: 10px">First Name: ${email.firstName}</div>
+                <div style="margin-bottom: 10px">Second Name: ${email.lastName}</div>
+                <div style="margin-bottom: 10px">Client Email: ${email.email}</div>
+                <div style="margin-bottom: 10px">Message: ${email.message}</div>
+                </b>`
     });
     console.log("Email sent");
     return promise;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const email = require('./email');
-
+const bodyParser = require('body-parser')
 const express = require('express');
-const app = express();
 const serverless = require("serverless-http");
 const router = express.Router();
+const app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.use(bodyParser.json())
 
 
-router.get("/", (req, res) => {
-    email.sendEmail().then(()=>{
+router.post("/", (req, res) => {
+    email.sendEmail(req.body).then(()=>{
         return res.json({
             success: 'Email has been sent successfully'
         });
-    })
+    }).catch((error) => {
+        return error;
+    });
 });
 
 
